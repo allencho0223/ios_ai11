0e3c3c96c660c62f4a0b19bb2c44e2c40071fd83
allencho <allen.my.cho@gmail.com>
2019-02-12 23:08:27 +1100

    Practicing view control with some UIs

---
+++ ai 11/ai 11/FirstViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FirstViewController.swift
+//  ai 11
+//
+//  Created by Allen cho on 12/02/2019.
+//  Copyright © 2019 Allen cho. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "goToSecondViewController", sender: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ai 11/ai 11/SecondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SecondViewController.swift
+//  ai 11
+//
+//  Created by Allen cho on 12/02/2019.
+//  Copyright © 2019 Allen cho. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func goBack(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
